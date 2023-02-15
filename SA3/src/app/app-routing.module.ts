6c51c80149435995b2444c04e45ba901c9b4b24a
sovietkilla07 <sovietmoderator109@gmail.com>
2023-02-14 21:52:57 -0300

    agora sim o negocio ta ficando bom

--- SA3/src/app/app-routing.module.ts
+++ SA3/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContatoComponent } from './views/contato/contato.component';
 import { LoginComponent } from './views/login/login.component';
-import { HeaderComponent } from './views/header/header.component';
 
 const routes: Routes = [
   {path: "contato", component: ContatoComponent},
   {path: "login", component: LoginComponent},
-  {path: "header", component: HeaderComponent}
 
 ];
 
